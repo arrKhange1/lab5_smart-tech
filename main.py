a30862b1eb97e426531d151c703e59021dfcfd00
Arthur <therock65frostik@gmail.com>
2023-11-07 19:48:40 +0500

    refact: change indents and add some beautiful prints

--- main.py
+++ main.py
@@ -10,10 +10,16 @@ brother(X,Y) <= (father(Z,X)) & (father(Z,Y)) & ~(X==Y)
 cousin(X,Y) <= (father(Z,X)) & (father(W,Y)) & (brother(Z,W))
 grandson(X,Y) <= (father(Y,Z)) & (father(Z,X))
 descendent(X,Y) <= (father(Y,X))
+print('кто брат для "c"?')
 print(brother(X,'c')) # кто брат для 'c'?
-print('|----|')
+print('\n')
+print('все кузины в дереве')
 print(cousin(X,Y)) # все кузины в дереве
-print('|----|')
+print('\n')
+print('для кого "e" является внуком?')
 print(grandson('e',Y)) # для кого 'e' внук?
-print('|----|')
+print('\n')
+print('кто потомки "a"?')
 print(descendent(X, 'a')) # кто потомки 'a'?
+
+
